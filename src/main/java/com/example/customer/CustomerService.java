90ab609a0b76f10de8ef9a49d312f702033fb1aa
Trishna Chakraborty <trishnachakraborty13@gmail.com>
2019-07-08 17:35:52 +0600

    manual acknowkledgement is off

--- src/main/java/com/example/customer/CustomerService.java
+++ src/main/java/com/example/customer/CustomerService.java
@@ -4,6 +4,7 @@ package com.example.customer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rabbitmq.client.*;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.connection.ChannelProxy;
 import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Header;
@@ -42,12 +43,13 @@ public class CustomerService {
 
     @RabbitListener(queues = "postCustomer" )
     @SendTo("reply_queue")
-    public  String postCustomer(String str, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws IOException{
+    public  String postCustomer(String str,  Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws IOException{
        ObjectMapper objectMapper= new ObjectMapper();
         Customer customer=objectMapper.readValue(str,Customer.class);
         System.out.println("Got request to post"+ customer);
         customerRepository.save(customer);
-        channel.basicAck(tag,false);
+        //Channel actualChannel = ((ChannelProxy) channel).getTargetChannel();
+        //channel.basicAck(tag,false);
         System.out.println("Sent response from post"+ customer);
         return str;
 
@@ -59,8 +61,9 @@ public class CustomerService {
         Customer customer=objectMapper.readValue(str,Customer.class);
         System.out.println("Got request to update "+ customer);
        // customerRepository.deleteById(customer.getId());
+
         customerRepository.save(customer);
-        channel.basicAck(tag,false);
+        //channel.basicAck(tag,false);
         System.out.println("Sent response from update"+ customer);
          return str;
 
